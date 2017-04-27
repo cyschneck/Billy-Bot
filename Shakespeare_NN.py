df8ebb1b58e5d7e7155c5308844a93ca328deff7
cschneck <cora.schneck@gmail.com>
2017-04-27 00:53:31 -0600

    set up for character:speech dictionary

--- Shakespeare_NN.py
+++ Shakespeare_NN.py
@@ -19,9 +19,58 @@ punc = ['!', ',', '.', ':', '\'', ';', '*', '--']
 character_names = ['claudius', 'hamlet', 'polonius', 'horatio', 'laertes',
 					'voltimand', 'cornelius', 'rosencrantz', 'guildenstern',
 					'osric', 'gentleman', 'priest', 'marcellus', 'bernardo',
-					'francisco', 'reynaldo', 'players', 'clown1', 'clown2',
+					'francisco', 'reynaldo', 'players', 'clownone', 'clowntwo',
 					'gertrude', 'ophelia', 'fortinbras', 'captain', 'ambassadors', 'ghost', 'other']
+########################################################################
+## SETTING UP THE DICTIONARIES FROM THE GIVEN FILES
 
+def readingFileDict(filename):
+	# reads in the file and returns a dictionay with headers and sequences: {header:sequence}
+	fullList = []
+	characterList = []
+	char_list = []
+	speech_list = []
+
+	append = fullList.append # avoid re-using append (to improve running time)
+	with open(filename, "r") as given_file:
+		seq = ''
+		for line in given_file:
+			line = line.rstrip('\n').replace("\t", "@").replace("\r", "@") # replace spaces with known character and replace tabs
+			if line.startswith('>'):
+				line = line + ' '
+				line = line.replace('>', ' >')
+			append(line)
+		seq = ''.join(fullList).upper()
+		characterList = seq.split()
+		# Pulls out the sequences and genomes
+		# By removing any extranous puncutation with predicatble characters to be spliced
+
+		# Returns the header sequence name
+		append = char_list.append # avoid re-using append (to improve running time)
+		for element in characterList:
+			if ">" in element:
+				element = element.replace("@", "")
+				element = element.strip(">") # assumes all header/sequences starts with >
+				append(element) # returns a list of  headers ['chrI', 'chrII', etc...]
+
+		speech_list =  [x for x in characterList if '>' not in x] # returns a list of sequence ['ATC', 'TGGC', etc..]
+		speech_list = map(str.lower, speech_list) # convert all sequences to upper case for consitency
+
+		for i in range(len(speech_list)): # remove extra @ left behind by replace and replace n with A (n = any ATCG)
+			if '@' in speech_list[i]:
+				new_value = speech_list[i].replace("@", "")
+				speech_list[i] = new_value
+		seq_dict = seqDictPairs(char_list, speech_list) # tuples of a pair's list and a dictionary {seq:gen}
+	return seq_dict
+
+def seqDictPairs(header_list, sequence_list):
+	# creates a dictionary between the sequence (header) and the associated genome {seq:genome} dictionary
+	# takes in the header list and the sequence list to combine into a single diectionary that can be searched
+	# setting up sequence genome dictionary
+	seq_gen_dict = {}
+	seq_gen_dict = zip(header_list, sequence_list) # combine the two lists
+	seq_gen_dict = dict(seq_gen_dict) # create new dictionary from the lists
+	return seq_gen_dict
 
 def printDict(dictionary):
 	#print from largest to smallest frequency in order
@@ -62,52 +111,6 @@ def basicNN():
 	# basic nueral network trained for sentiment
 	pass
 
-def readingFileDict(filename):
-	# reads in the file and returns a dictionay with headers and sequences: {header:sequence}
-	fullList = []
-	Charlist = []
-	character_list = []
-	speech_list = []
-
-	append = fullList.append # avoid re-using append (to improve running time)
-	with open(filename, "r") as given_file:
-		seq = ''
-		for line in given_file:
-			line = line.rstrip('\n').replace(" ", "@").replace("\t", "@").replace("\r", "@") # replace spaces with known character and replace tabs
-			if line.startswith('>'):
-				line = line + ' '
-				line = line.replace('>', ' >')
-			append(line)
-		seq = ''.join(fullList).upper()
-		Charlist = seq.split()
-		# Pulls out the character headers and speeches
-		# By removing any extranous puncutation with predictable characters to be spliced
-
-		# Returns the header sequence name
-		append = seqlist.append # avoid re-using append (to improve running time)
-		for element in Charlist:
-			if ">" in element:
-				element = element.replace("@", "")
-				element = element.strip(">") # assumes all header/sequences starts with >
-				append(element) # returns a list of  headers ['chrI', 'chrII', etc...]
-
-		speech_list =  [x for x in Charlist if '>' not in x] # returns a list of sequence ['ATC', 'TGGC', etc..]
-		speech_list = map(str.upper, speech_list) # convert all sequences to upper case for consitency
-
-		for i in range(len(speech_list)): # remove extra @ left behind by replace and replace n with A (n = any ATCG)
-			if '@' in speech_list[i]:
-				new_value = speech_list[i].replace("@", "")
-				speech_list[i] = new_value
-		seq_dict = seqDictPairs(character_list, speech_list) # tuples of a pair's list and a dictionary {seq:gen}
-	return seq_dict
-
-def seqDictPairs(character_list, speech_list):
-	# creates a dictionary between the charcter (header) and the associated speech {character:speech} dictionary
-	char_spe_dict = {}
-	char_spe_dict = zip(character_list, speech_list) # combine the two lists
-	char_spe_dict = dict(char_spe_dict) # create new dictionary from the lists
-	return char_spe_dict
-
 if __name__ == '__main__':
 	import argparse
 	parser = argparse.ArgumentParser(description="flag format given as: -F <filename>")
@@ -123,12 +126,14 @@ if __name__ == '__main__':
 			exit()
 	
 	char_speech_dict = readingFileDict(filename)
+	print(char_speech_dict)
+	'''
 	words = "thus conscience does make cowards of us all"
-	
 	text = TextBlob(words)
 	print(text.tags)
 	print(text.words)
 	print(text.sentiment)
+	'''
 
 	# iterate through all tokens for each speech (50 or 100 tokens in size max)
 	# store sentiment in list for each character to graph
