ce983bbefd5e05bbf20a55fcf92d85db65d62497
unaschneck <schneck.una@gmail.com>
2017-05-31 14:09:15 -0700

    Update shakespeare_sentiment.py

--- shakespeare_sentiment.py
+++ shakespeare_sentiment.py
@@ -532,9 +532,11 @@ if __name__ == '__main__':
 	plt.show()
 	
 	# Save the figure as a jpeg (first as a png and converted to jeg)
-	fig_name = "%chart_title.png" %chart_title # file name will have the same name as the chart title
+	fig_name = "{0}".format(chart_title) # file name will have the same name as the chart title
     	plt.savefig(fig_name, format='png') # savefig doesn't directly support jpeg format
 	Image.open(fig_name).save(fig_name','JPEG') # conversion to supported jpeg
 				  
 	# include when a character enters and exit the play, how often they speech (frequency/total play)
 	# fix spacing after ; and with carriage returns (needs space)
+				  #
+
